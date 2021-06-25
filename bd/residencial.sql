c3ed3aef4b1f4afe4416d632dee13c8f0dae41fb
VICTOR JOAQUIN LOPEZ CASTILLO <48496874+victorLopz@users.noreply.github.com>
2021-06-25 02:23:11 -0600

    La Base de datos

---
+++ bd/residencial.sql
@@ -0,0 +1,647 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 25-06-2021 a las 10:17:47
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `residencial`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacen`
+--
+
+CREATE TABLE `almacen` (
+  `IDCodigoAlmacen` int(11) NOT NULL,
+  `NombreArticulo` varchar(50) NOT NULL,
+  `Marca` varchar(50) NOT NULL,
+  `Modelopresentacion` varchar(50) DEFAULT NULL,
+  `Codigo1` varchar(50) NOT NULL,
+  `Codigo2` varchar(50) DEFAULT NULL,
+  `Notas` varchar(400) DEFAULT NULL,
+  `precioVenta` double NOT NULL,
+  `IsVisible` int(11) NOT NULL DEFAULT 1,
+  `PrecioCompra` double NOT NULL,
+  `Stock` int(11) DEFAULT NULL,
+  `TimeSpace` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `almacen`
+--
+
+INSERT INTO `almacen` (`IDCodigoAlmacen`, `NombreArticulo`, `Marca`, `Modelopresentacion`, `Codigo1`, `Codigo2`, `Notas`, `precioVenta`, `IsVisible`, `PrecioCompra`, `Stock`, `TimeSpace`) VALUES
+(1, 'Gaseosa', 'Coca Cola', 'medio litro', '0987457', '', '', 20, 1, 10, 100, '2021-06-25 01:35:30'),
+(2, 'computadora', 'Dell', 'Core i9+', '8643325788', '', '', 12000, 1, 10000, 100, '2021-06-25 01:36:04'),
+(3, 'Cargador', 'Huawei', '22.5 Wts', '78432367789', '', '', 800, 1, 500, 100, '2021-06-25 01:36:36');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacencuatro`
+--
+
+CREATE TABLE `almacencuatro` (
+  `IDAlmacencuatro` int(11) NOT NULL,
+  `IDCodigoAlmacenPK` int(11) NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `Notas` varchar(50) NOT NULL,
+  `isVisible` int(11) NOT NULL DEFAULT 1,
+  `TimeSpace` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `almacencuatro`
+--
+
+INSERT INTO `almacencuatro` (`IDAlmacencuatro`, `IDCodigoAlmacenPK`, `Stock`, `Notas`, `isVisible`, `TimeSpace`) VALUES
+(1, 1, 0, '', 1, '2021-06-25 07:35:30'),
+(2, 2, 0, '', 1, '2021-06-25 07:36:05'),
+(3, 3, 0, '', 1, '2021-06-25 07:36:36');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacendos`
+--
+
+CREATE TABLE `almacendos` (
+  `IDAlmacendos` int(11) NOT NULL,
+  `IDCodigoAlmacenPK` int(11) NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `Notas` varchar(50) NOT NULL,
+  `isVisible` int(11) NOT NULL DEFAULT 1,
+  `TimeSpace` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `almacendos`
+--
+
+INSERT INTO `almacendos` (`IDAlmacendos`, `IDCodigoAlmacenPK`, `Stock`, `Notas`, `isVisible`, `TimeSpace`) VALUES
+(1, 1, 0, '', 1, '2021-06-25 07:35:30'),
+(2, 2, 0, '', 1, '2021-06-25 07:36:04'),
+(3, 3, 0, '', 1, '2021-06-25 07:36:36');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacentres`
+--
+
+CREATE TABLE `almacentres` (
+  `IDAlmacentres` int(11) NOT NULL,
+  `IDCodigoAlmacenPK` int(11) NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `Notas` varchar(50) NOT NULL,
+  `isVisible` int(11) NOT NULL DEFAULT 1,
+  `TimeSpace` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `almacentres`
+--
+
+INSERT INTO `almacentres` (`IDAlmacentres`, `IDCodigoAlmacenPK`, `Stock`, `Notas`, `isVisible`, `TimeSpace`) VALUES
+(1, 1, 0, '', 1, '2021-06-25 07:35:30'),
+(2, 2, 0, '', 1, '2021-06-25 07:36:04'),
+(3, 3, 0, '', 1, '2021-06-25 07:36:36');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacenuno`
+--
+
+CREATE TABLE `almacenuno` (
+  `IDAlmacenuno` int(11) NOT NULL,
+  `IDCodigoAlmacenPK` int(11) NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `Notas` varchar(50) NOT NULL,
+  `isVisible` int(11) NOT NULL DEFAULT 1,
+  `TimeSpace` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `almacenuno`
+--
+
+INSERT INTO `almacenuno` (`IDAlmacenuno`, `IDCodigoAlmacenPK`, `Stock`, `Notas`, `isVisible`, `TimeSpace`) VALUES
+(1, 1, 100, '', 1, '2021-06-25 07:39:54'),
+(2, 2, 100, '', 1, '2021-06-25 07:36:04'),
+(3, 3, 100, '', 1, '2021-06-25 07:36:36');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `catalogodatos`
+--
+
+CREATE TABLE `catalogodatos` (
+  `IDcatalogoDatos` int(11) NOT NULL,
+  `Primer_Nombre_Empresa` varchar(50) NOT NULL,
+  `Apellidos` varchar(50) DEFAULT NULL,
+  `NumeroRUC_Cedula` varchar(20) NOT NULL,
+  `Direccion` varchar(1000) NOT NULL,
+  `Numero_Telefonico` int(11) NOT NULL,
+  `Email` varchar(20) DEFAULT NULL,
+  `IsVisible` int(11) NOT NULL DEFAULT 1,
+  `Comentario` varchar(1000) DEFAULT NULL,
+  `TimeSpace` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `IDClientes` int(11) NOT NULL,
+  `IDcatalogoDatosPK` int(11) NOT NULL,
+  `IDEstadoClientePK` int(11) NOT NULL,
+  `Comentario` varchar(300) NOT NULL,
+  `IsVisible` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `descuento`
+--
+
+CREATE TABLE `descuento` (
+  `idDescuento` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT 0,
+  `Comentario` varchar(50) NOT NULL,
+  `timespace` date NOT NULL DEFAULT current_timestamp(),
+  `almacen` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `descuento`
+--
+
+INSERT INTO `descuento` (`idDescuento`, `estado`, `Comentario`, `timespace`, `almacen`) VALUES
+(1, 0, 'Almacen 1', '0000-00-00', 1),
+(2, 0, 'Almacen 2', '2021-03-29', 2),
+(3, 0, 'Almacen 3', '2021-03-29', 3),
+(4, 0, 'Almacen 4', '2021-03-29', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalledefactura`
+--
+
+CREATE TABLE `detalledefactura` (
+  `IDFacturaDetalle` int(11) NOT NULL,
+  `Unidades` int(11) NOT NULL,
+  `producto` int(11) NOT NULL,
+  `Precio` double NOT NULL,
+  `IDFacturaPK` int(11) NOT NULL,
+  `TimeSpace` date NOT NULL DEFAULT current_timestamp(),
+  `preciounit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `detalledefactura`
+--
+
+INSERT INTO `detalledefactura` (`IDFacturaDetalle`, `Unidades`, `producto`, `Precio`, `IDFacturaPK`, `TimeSpace`, `preciounit`) VALUES
+(1, 10, 1, 200, 1, '2021-06-25', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallesinventario`
+--
+
+CREATE TABLE `detallesinventario` (
+  `IDDetalleINV` int(11) NOT NULL,
+  `IDArticuloPK` int(11) NOT NULL,
+  `CantIngresada` int(11) NOT NULL,
+  `CostoUnitario` double NOT NULL,
+  `PrecioVenta/Unitario` double NOT NULL,
+  `IDIngresoINVPK` int(11) NOT NULL,
+  `Comentario` varchar(200) NOT NULL,
+  `IsVisible` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estadocliente`
+--
+
+CREATE TABLE `estadocliente` (
+  `IDEstadoCliente` int(11) NOT NULL,
+  `Comentario` varchar(500) NOT NULL,
+  `Numero_de_Estado` int(11) NOT NULL,
+  `SystemName` varchar(5000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `IDFactura` int(11) NOT NULL,
+  `Timespace` date NOT NULL DEFAULT current_timestamp(),
+  `AlmacenNumero` int(11) NOT NULL,
+  `SubTotal` double DEFAULT NULL,
+  `Iva` tinyint(1) DEFAULT NULL,
+  `Total` double NOT NULL,
+  `fechaEmision` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `montopagado` double NOT NULL,
+  `cambio` double NOT NULL,
+  `IsVisible` int(11) NOT NULL DEFAULT 1,
+  `descuento` int(11) NOT NULL,
+  `codigoRUCcedula` varchar(50) DEFAULT NULL,
+  `cliente` varchar(50) NOT NULL,
+  `tipofac` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `factura`
+--
+
+INSERT INTO `factura` (`IDFactura`, `Timespace`, `AlmacenNumero`, `SubTotal`, `Iva`, `Total`, `fechaEmision`, `montopagado`, `cambio`, `IsVisible`, `descuento`, `codigoRUCcedula`, `cliente`, `tipofac`) VALUES
+(1, '2021-06-25', 1, 0, NULL, 200, '2021-06-25 07:38:09', 300, 100, 1, 0, '001-030199-0000S', 'Joaquin De Los Angeles', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historialstock`
+--
+
+CREATE TABLE `historialstock` (
+  `IDhis` int(11) NOT NULL,
+  `producto` int(11) NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `TimeSpace` date NOT NULL,
+  `Horainsertada` datetime NOT NULL,
+  `Numeroalmacen` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `historialstock`
+--
+
+INSERT INTO `historialstock` (`IDhis`, `producto`, `Stock`, `TimeSpace`, `Horainsertada`, `Numeroalmacen`) VALUES
+(1, 1, 10, '2021-06-25', '2021-06-25 01:39:54', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingresoinventario`
+--
+
+CREATE TABLE `ingresoinventario` (
+  `IDIngresoINV` int(11) NOT NULL,
+  `Timespace` datetime NOT NULL DEFAULT current_timestamp(),
+  `Comentario` varchar(500) NOT NULL,
+  `MontoPrecio/Costo` double NOT NULL,
+  `IsVisible` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles_usuarios`
+--
+
+CREATE TABLE `roles_usuarios` (
+  `IDRolControlUsuariosPK` int(11) NOT NULL,
+  `RolUsuario` varchar(50) NOT NULL,
+  `Comentario` varchar(1000) DEFAULT NULL,
+  `Nivel` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `roles_usuarios`
+--
+
+INSERT INTO `roles_usuarios` (`IDRolControlUsuariosPK`, `RolUsuario`, `Comentario`, `Nivel`) VALUES
+(1, 'Administrador', 'Control del sistema total', 1),
+(2, 'Trabajador Almacen 1', 'Trabajador del almacen 1', 2),
+(3, 'Trabajador Almacen 2', 'Trabajador del almacen 2', 3),
+(4, 'Trabajador Almacen 3', 'Trabajador del almacen 3', 4),
+(5, 'Trabajador Almacen 4', 'Trabajador del almacen 4', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipoalmacenes`
+--
+
+CREATE TABLE `tipoalmacenes` (
+  `IDalmacentipo` int(11) NOT NULL,
+  `NombreAlmacen` varchar(50) NOT NULL,
+  `Comentario` varchar(200) NOT NULL,
+  `Nivel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tipoalmacenes`
+--
+
+INSERT INTO `tipoalmacenes` (`IDalmacentipo`, `NombreAlmacen`, `Comentario`, `Nivel`) VALUES
+(1, 'Almacen 1', 'Este es el primer almacen', 1),
+(2, 'Almacen 2', 'Este es el segundo almacen', 2),
+(3, 'Almacen 3', 'Este es el Tercer almacen', 3),
+(4, 'Almacen 4', 'Este es el Cuarto almacen', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipocambio`
+--
+
+CREATE TABLE `tipocambio` (
+  `IDcambio` int(11) NOT NULL,
+  `TipoMoneda` varchar(20) NOT NULL,
+  `Valor` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tipocambio`
+--
+
+INSERT INTO `tipocambio` (`IDcambio`, `TipoMoneda`, `Valor`) VALUES
+(1, 'Dolar EstadoUnidense', 35.12);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `IdUsuarios` int(11) NOT NULL,
+  `IDcatalogoDatosPK` int(11) DEFAULT NULL,
+  `IDRolControlUsuariosPK` int(11) NOT NULL COMMENT 'Campos de roles para control de Usuarios',
+  `UserName` varchar(40) NOT NULL,
+  `PasswordName` varchar(50) NOT NULL,
+  `TimeSpace` datetime NOT NULL DEFAULT current_timestamp(),
+  `IsVisible` int(11) NOT NULL DEFAULT 1,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`IdUsuarios`, `IDcatalogoDatosPK`, `IDRolControlUsuariosPK`, `UserName`, `PasswordName`, `TimeSpace`, `IsVisible`, `estado`) VALUES
+(1, 1, 1, 'admin@admin.com', 'demo', '2021-02-21 23:14:05', 1, 0),
+(2, NULL, 2, 'demo@admin.com', 'demo', '2021-03-01 22:13:39', 1, 0),
+(3, NULL, 3, 'lubricantes2@admin.com', 'mobil2018', '2021-03-07 11:25:41', 1, 0),
+(4, NULL, 4, 'lubricantes3@admin.com', 'shell2018', '2021-03-07 13:14:42', 1, 0),
+(5, NULL, 5, 'lubricantes4@admin.com', 'cat2018', '2021-03-31 22:10:20', 1, 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `almacen`
+--
+ALTER TABLE `almacen`
+  ADD PRIMARY KEY (`IDCodigoAlmacen`);
+
+--
+-- Indices de la tabla `almacencuatro`
+--
+ALTER TABLE `almacencuatro`
+  ADD PRIMARY KEY (`IDAlmacencuatro`);
+
+--
+-- Indices de la tabla `almacendos`
+--
+ALTER TABLE `almacendos`
+  ADD PRIMARY KEY (`IDAlmacendos`);
+
+--
+-- Indices de la tabla `almacentres`
+--
+ALTER TABLE `almacentres`
+  ADD PRIMARY KEY (`IDAlmacentres`);
+
+--
+-- Indices de la tabla `almacenuno`
+--
+ALTER TABLE `almacenuno`
+  ADD PRIMARY KEY (`IDAlmacenuno`);
+
+--
+-- Indices de la tabla `catalogodatos`
+--
+ALTER TABLE `catalogodatos`
+  ADD PRIMARY KEY (`IDcatalogoDatos`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`IDClientes`);
+
+--
+-- Indices de la tabla `descuento`
+--
+ALTER TABLE `descuento`
+  ADD PRIMARY KEY (`idDescuento`);
+
+--
+-- Indices de la tabla `detalledefactura`
+--
+ALTER TABLE `detalledefactura`
+  ADD PRIMARY KEY (`IDFacturaDetalle`);
+
+--
+-- Indices de la tabla `detallesinventario`
+--
+ALTER TABLE `detallesinventario`
+  ADD PRIMARY KEY (`IDDetalleINV`);
+
+--
+-- Indices de la tabla `estadocliente`
+--
+ALTER TABLE `estadocliente`
+  ADD PRIMARY KEY (`IDEstadoCliente`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`IDFactura`);
+
+--
+-- Indices de la tabla `historialstock`
+--
+ALTER TABLE `historialstock`
+  ADD PRIMARY KEY (`IDhis`);
+
+--
+-- Indices de la tabla `ingresoinventario`
+--
+ALTER TABLE `ingresoinventario`
+  ADD PRIMARY KEY (`IDIngresoINV`);
+
+--
+-- Indices de la tabla `roles_usuarios`
+--
+ALTER TABLE `roles_usuarios`
+  ADD PRIMARY KEY (`IDRolControlUsuariosPK`);
+
+--
+-- Indices de la tabla `tipoalmacenes`
+--
+ALTER TABLE `tipoalmacenes`
+  ADD PRIMARY KEY (`IDalmacentipo`);
+
+--
+-- Indices de la tabla `tipocambio`
+--
+ALTER TABLE `tipocambio`
+  ADD PRIMARY KEY (`IDcambio`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`IdUsuarios`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `almacen`
+--
+ALTER TABLE `almacen`
+  MODIFY `IDCodigoAlmacen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `almacencuatro`
+--
+ALTER TABLE `almacencuatro`
+  MODIFY `IDAlmacencuatro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `almacendos`
+--
+ALTER TABLE `almacendos`
+  MODIFY `IDAlmacendos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `almacentres`
+--
+ALTER TABLE `almacentres`
+  MODIFY `IDAlmacentres` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `almacenuno`
+--
+ALTER TABLE `almacenuno`
+  MODIFY `IDAlmacenuno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `catalogodatos`
+--
+ALTER TABLE `catalogodatos`
+  MODIFY `IDcatalogoDatos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `IDClientes` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `descuento`
+--
+ALTER TABLE `descuento`
+  MODIFY `idDescuento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `detalledefactura`
+--
+ALTER TABLE `detalledefactura`
+  MODIFY `IDFacturaDetalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `detallesinventario`
+--
+ALTER TABLE `detallesinventario`
+  MODIFY `IDDetalleINV` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estadocliente`
+--
+ALTER TABLE `estadocliente`
+  MODIFY `IDEstadoCliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `IDFactura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `historialstock`
+--
+ALTER TABLE `historialstock`
+  MODIFY `IDhis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `ingresoinventario`
+--
+ALTER TABLE `ingresoinventario`
+  MODIFY `IDIngresoINV` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles_usuarios`
+--
+ALTER TABLE `roles_usuarios`
+  MODIFY `IDRolControlUsuariosPK` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `tipoalmacenes`
+--
+ALTER TABLE `tipoalmacenes`
+  MODIFY `IDalmacentipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tipocambio`
+--
+ALTER TABLE `tipocambio`
+  MODIFY `IDcambio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `IdUsuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
